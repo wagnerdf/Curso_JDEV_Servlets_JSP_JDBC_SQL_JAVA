4adb6ba4641551d4a9cd9edd56f3e5d2e89cb149
Wagner Andrade <wagnerandrade_df@hotmail.com>
2021-08-25 16:02:55 -0300

    Criando a Servlet

---
+++ src/main/java/servlets/serveletLogin.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/serveletLogin")/*Mapeamento de URL que vem da tela*/
+public class serveletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+   
+    public serveletLogin() {
+       
+    }
+
+	
+    /*Recebe os dados pela URL em parametros*/
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/*Recebe os dados enviados por um formulário*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("idade"));
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Curso JSP</title>
+</head>
+<body>
+
+<h1>Bem vindo ao curoso de JSP</h1>
+
+<form action="serveletLogin" method="post">
+
+<input name="nome">
+<input name="idade">
+
+<input type="submit" value="Enviar">
+
+</form>
+
+
+
+
+</body>
+</html>
