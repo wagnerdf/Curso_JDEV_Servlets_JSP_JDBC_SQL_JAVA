4959f3348cd97bce1eff84361d997c5ba0cc60ab
Wagner Andrade <wagnerandrade_df@hotmail.com>
2021-08-27 12:11:47 -0300

    Simulação de login no sistema

--- src/main/java/servlets/serveletLogin.java
+++ src/main/java/servlets/serveletLogin.java
@@ -37,6 +37,19 @@ public class serveletLogin extends HttpServlet {
 			ModelLogin modelLogin = new ModelLogin(); 
 			modelLogin.setLogin(login);
 			modelLogin.setSenha(senha);
+			
+			if (modelLogin.getLogin().equalsIgnoreCase("admin") && modelLogin.getSenha().equalsIgnoreCase("admin") ) { /*Simulando login*/
+				request.getSession().setAttribute("usuario", modelLogin.getLogin());
+				
+				RequestDispatcher redirecionar = request.getRequestDispatcher("principal/principal.jsp");
+				redirecionar.forward(request, response);
+				
+			}else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Informe o login e senha corretamente!");
+				redirecionar.forward(request, response);
+			}
+			
 		}else {
 			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
 			request.setAttribute("msg", "Informe o login e senha corretamente!");
---
+++ src/main/webapp/principal/principal.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Bem vindo ao Sistema ******** </title>
+</head>
+<body>
+
+<h1>Tela principal após o login</h1>
+
+</body>
+</html>
