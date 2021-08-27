e8d7d775c68f3e065fffe82ab97bb9a96def9729
Wagner Andrade <wagnerandrade_df@hotmail.com>
2021-08-27 10:51:08 -0300

    Estrutura Back-End com os Pack

--- src/main/java/servlets/serveletLogin.java
+++ src/main/java/servlets/serveletLogin.java
@@ -22,7 +22,7 @@ public class serveletLogin extends HttpServlet {
     /*Recebe os dados pela URL em parametros*/
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	/*Recebe os dados enviados por um formulário*/
