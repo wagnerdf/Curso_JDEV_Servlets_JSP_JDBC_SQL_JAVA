461860f83e5833fce9b671d02c587f69f07910cc
Wagner Andrade <wagnerandrade_df@hotmail.com>
2021-08-27 11:44:49 -0300

    Criando Model de Login

--- src/main/java/servlets/serveletLogin.java
+++ src/main/java/servlets/serveletLogin.java
@@ -2,11 +2,13 @@ package servlets;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
 
 
 @WebServlet("/serveletLogin")/*Mapeamento de URL que vem da tela*/
@@ -28,8 +30,21 @@ public class serveletLogin extends HttpServlet {
 	/*Recebe os dados enviados por um formulário*/
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		System.out.println(request.getParameter("nome"));
-		System.out.println(request.getParameter("idade"));
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+			ModelLogin modelLogin = new ModelLogin(); 
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+		}else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe o login e senha corretamente!");
+			redirecionar.forward(request, response);
+		}
+		
+		
+		
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -12,14 +12,25 @@
 
 <form action="serveletLogin" method="post">
 
-<input name="nome">
-<input name="idade">
+<table>
+	<tr>
+		<td><label>Login</label></td>
+		<td><input name="login" type="text"></td>
+	</tr>
+	<tr>
+		<td><label>Senha</label></td>
+		<td><input name="senha" type="password"></td>
+	</tr>
+	<tr>
+	<td/>	
+		<td><input type="submit" value="Enviar"></td>
+	</tr>
 
-<input type="submit" value="Enviar">
+</table>
 
 </form>
 
-
+<h4>${msg}</h4>
 
 
 </body>
